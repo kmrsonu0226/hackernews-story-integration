4b3fcb50068361c2f41f9812762c9424c1617195
sonu <sonu.kumar@lifcare.in>
2020-05-31 19:01:57 +0530

    update swagger config

--- src/main/java/com/story/integration/config/SwaggerConfig.java
+++ src/main/java/com/story/integration/config/SwaggerConfig.java
@@ -1,8 +1,5 @@
 package com.story.integration.config;
 
-import java.util.Arrays;
-import java.util.HashSet;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
